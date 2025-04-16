1fab0348071d3d4c41d69859bb4a42b315d98e62
thearunkumar07 <arun.franchisebhoomi@gmail.com>
2025-04-16 17:25:18 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -36,6 +36,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
+          </Analytics>
         </ThemeProvider>
       </body>
     </html>
